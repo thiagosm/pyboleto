6b03d577f0d6a0c6fd702871415754c8b4fb7df9
Thiago Montenegro <opencode@gmail.com>
2019-01-10 08:51:59 -0300

    ajuste cedred

--- pyboleto/bank/cecred.py
+++ pyboleto/bank/cecred.py
@@ -25,7 +25,7 @@ class BoletoCecred(BoletoData):
 
     @property
     def campo_livre(self):
-        content = "%6s%8s%9s%2s"  % (str(self.agencia_cedente).zfill(6),
+        content = "%6s%8s%9s%2s"  % (str(self.convenio).zfill(6),
                                      str(self.conta_cedente).zfill(8),
                                      str(self.numero_documento).zfill(9),
                                      self.carteira)
