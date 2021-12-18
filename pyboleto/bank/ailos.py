6f51d5e4d509697efd6680c8c82af2183d947016
Junior Andrade <seisxis@gmail.com>
2021-12-18 13:00:55 -0300

    banco iolos/crecred

--- pyboleto/bank/ailos.py
+++ pyboleto/bank/ailos.py
@@ -3,30 +3,47 @@ from ..data import BoletoData, CustomProperty
 
 
 class BoletoAilos(BoletoData):
-    """
-      Boleto Ailos
-    """
-    agencia_cedente = CustomProperty('agencia_cedente', 6)
-    conta_cedente = CustomProperty('conta_cedente', 8)
-    nosso_numero = CustomProperty('nosso_numero', 7)
+
+    nosso_numero = CustomProperty('nosso_numero', 10)
+    agencia_cedente = CustomProperty('agencia_cedente', 4)
+    conta_cedente = CustomProperty('conta_cedente', 10)
+    carteira = CustomProperty('carteira',2)
 
     def __init__(self):
-        BoletoData.__init__(self)
+        super(BoletoAilos, self).__init__()
+
         self.codigo_banco = "085"
         self.logo_image = "logo_ailos.jpg"
+        self.carteira = '01'
+
+
+    def format_nosso_numero(self):
+        return '%10s-%1s' %(self.nosso_numero.zfill(10),self.dv_nosso_numero)
+
+
+    @property
+    def dv_nosso_numero(self):
+        _nn = self.nosso_numero.zfill(10)
+        resto2 = self.modulo11(_nn, 7, 1)
+        digito = 11 - resto2
+
+        if digito > 9:
+            dv = 0
+        else:
+            dv = digito
+        return dv
 
 
     @property
     def agencia_conta_cedente(self):
-        return "%s/%s" % (self.agencia_cedente, self.conta_cedente)
-
-    def format_nosso_numero(self):
-        return "%s%s" % (str(self.conta_cedente).zfill(8),str(self.numero_documento).zfill(9))
+        return "%s / %s-%s" % (
+            self.agencia_cedente.zfill(4),
+            self.conta_cedente[0:-1].zfill(9),self.conta_cedente[-1:])
 
     @property
     def campo_livre(self):
-        content = "%6s%8s%9s%2s"  % (str(self.convenio).zfill(6),
-                                     str(self.conta_cedente).zfill(8),
-                                     str(self.numero_documento).zfill(9),
-                                     self.carteira)
+        content = "%4s%10s%11s" % (self.agencia_cedente.zfill(4),
+                                   self.conta_cedente.zfill(10),
+                                   self.format_nosso_numero().replace('-',''))
         return str(content)
+
