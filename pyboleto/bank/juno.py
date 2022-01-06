6d1ab6b28f5e06d89c1fc3a0005304844a827ad4
Raylan <jeffersonraylan@gmail.com>
2022-01-06 11:52:48 -0300

    Ajuste no Juno - Permitir passar a linha digitável.

--- pyboleto/bank/juno.py
+++ pyboleto/bank/juno.py
@@ -15,8 +15,9 @@ class BoletoJuno(BoletoData):
 
         self.codigo_banco = "383"
         self.logo_image = "logo_juno.jpg"
-        self.carteira = '0000'
+        self.carteira = '0001'
         self.barcode_ = '' # pegar barcode do gateway de pagamento
+        self.linha_digitavel_ = '' # pegar linha_digitavel do gateway de pagamento
 
     @property
     def agencia_conta_cedente(self):
@@ -29,4 +30,21 @@ class BoletoJuno(BoletoData):
     def barcode(self):
         return self.barcode_
 
+    @property
+    def linha_digitavel(self):
+        if not self.linha_digitavel_:
+            linha = self.barcode
+            if not linha:
+                raise BoletoException("Boleto doesn't have a barcode")
 
+            def monta_campo(campo):
+                campo_dv = "%s%s" % (campo, self.modulo10(campo))
+                return "%s.%s" % (campo_dv[0:5], campo_dv[5:])
+
+            return ' '.join([monta_campo(linha[0:4] + linha[19:24]),
+                             monta_campo(linha[24:34]),
+                             monta_campo(linha[34:44]),
+                             linha[4],
+                             linha[5:19]])
+
+        return self.linha_digitavel_
