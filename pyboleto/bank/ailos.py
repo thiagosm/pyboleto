c648b55886cca40d6cf225a5eb90467d6a8d6221
Junior Andrade <seisxis@gmail.com>
2022-02-18 19:03:49 -0300

    ailos

--- pyboleto/bank/ailos.py
+++ pyboleto/bank/ailos.py
@@ -4,9 +4,9 @@ from ..data import BoletoData, CustomProperty
 
 class BoletoAilos(BoletoData):
 
-    nosso_numero = CustomProperty('nosso_numero', 10)
+    nosso_numero = CustomProperty('nosso_numero', 9)
     agencia_cedente = CustomProperty('agencia_cedente', 4)
-    conta_cedente = CustomProperty('conta_cedente', 10)
+    conta_cedente = CustomProperty('conta_cedente', 8)
     carteira = CustomProperty('carteira', 2)
 
     def __init__(self):
@@ -23,7 +23,8 @@ class BoletoAilos(BoletoData):
             self.conta_cedente[0:-1].zfill(9),self.conta_cedente[-1:])
 
     def format_nosso_numero(self):
-        return "%s%s" % (str(self.conta_cedente).zfill(8),str(self.numero_documento).zfill(9))
+        return "%s%s" % (str(self.conta_cedente).zfill(8), 
+                         str(self.numero_documento).zfill(9))
 
     @property
     def campo_livre(self):
