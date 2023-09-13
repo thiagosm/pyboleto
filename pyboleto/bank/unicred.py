8683cebcf51e256da408cff9c8655dfb87812cfa
Junior Andrade <seisxis@gmail.com>
2023-09-13 09:34:59 -0300

    ajuste unicred

--- pyboleto/bank/unicred.py
+++ pyboleto/bank/unicred.py
@@ -25,7 +25,7 @@ class BoletoUnicred(BoletoData):
     @property
     def dv_nosso_numero(self):
         _nn = self.nosso_numero.zfill(10)
-        resto2 = self.modulo11(_nn, self.modulo11_tamanho, 1)
+        resto2 = self.modulo11(_nn, int(self.modulo11_tamanho), 1)
         digito = 11 - resto2
 
         if digito > 9:
